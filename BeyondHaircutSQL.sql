dbb0fdeb9df23db998ec9dcae35a7941521c9cec
Maggie <maggiexu@ccs.neu.edu>
2015-04-26 22:10:22 -0700

    added sql update

--- BeyondHaircutSQL.sql
+++ BeyondHaircutSQL.sql
@@ -67,8 +67,8 @@ BarberId INT AUTO_INCREMENT,
 FirstName Varchar(255) NOT NULL,
 LastName Varchar(255) NOT NULL,
 Picture LONGBLOB,
-About Varchar(255) NOT NULL,
-Rating Enum('0','1','2','3','4','5') NOT NULL,
+About Varchar(255),
+Rating Enum('0','1','2','3','4','5'),
 Gender ENUM('Male', 'Female'),
 StoreId INT NOT NULL,
 CONSTRAINT pk_Barber_BarberId PRIMARY KEY (BarberId)
@@ -89,11 +89,11 @@ CREATE TABLE Appointment (
 StoreId INT NOT NULL,
 AppointmentId INT auto_increment,
 Date DATETIME NOT NULL,
-Duration Time,
+#Duration Time,
 CustomerId INT NOT NULL,
 BarberId INT NOT NULL,
 Style VARCHAR(255),
-Price INT,
+#Price INT,
 #ENUM('Flat-top','Butch-cut','mullet','Buzz-cut','Caesar-cut',
 #'Comb-over','Crew-cut','Bowl cut','Bob-cut',
 #'Bouffant','Long-wave','Long-straight','Short-straight',
@@ -126,7 +126,6 @@ CREATE TABLE Comment (
     #ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Bob Cut', 'Female', '20');
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Bouffant', 'Female', '30');
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Bowl Cut', 'Male', '15');
@@ -143,5 +142,29 @@ INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Mu
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Pony Hair', 'Female', '25');
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Short Straight', 'Female', '20');
 INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Short Wave', 'Female', '30');
-INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Not Sure', 'NA', '0');
+INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Price`) VALUES ('NA', '0');
+#INSERT INTO `beyondHairCut`.`Hairstyle` (`Style`, `Gender`, `Price`) VALUES ('Not Sure', 'NA', '0');
 #UPDATE `beyondHairCut`.`Hairstyle` SET `Gender`='Male' WHERE `Style`='duckass';
+
+ALTER TABLE `beyondHairCut`.`Hairstyle` 
+ADD COLUMN `Duration` INT(11) NULL AFTER `Price`;
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='50' WHERE `Style`='Bob Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='60' WHERE `Style`='Bouffant';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='30' WHERE `Style`='Bowl Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='20' WHERE `Style`='Butch Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='40' WHERE `Style`='Buzz Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='40' WHERE `Style`='Caeser Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='20' WHERE `Style`='Comb Over';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='50' WHERE `Style`='Crew Cut';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='20' WHERE `Style`='duckass';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='30' WHERE `Style`='Flat Top';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='40' WHERE `Style`='High Top';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='50' WHERE `Style`='Long Straight';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='30' WHERE `Style`='Long Wave';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='30' WHERE `Style`='Mullet';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='0' WHERE `Style`='NA';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='30' WHERE `Style`='Pony Hair';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='40' WHERE `Style`='Short Straight';
+UPDATE `beyondHairCut`.`Hairstyle` SET `Duration`='50' WHERE `Style`='Short Wave';
+
+INSERT INTO `beyondHairCut`.`Address` (`AddressId`, `Street`, `City`, `State`, `Zip`) VALUES ('0', 'NA', 'NA', 'NA', 'NA');
